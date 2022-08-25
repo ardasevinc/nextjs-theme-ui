3a2ddf30545795957ea7875618183ff6cdd00d34
Arda Sevinç <ardasevinc.technical@gmail.com>
2022-08-25 12:40:14 +0300

    initial template setup

---
+++ components/Meta.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <title>ThemeUI NextJS Template</title>
+      <meta
+        name="description"
+        content="ThemeUI Next.js template created by @ardasevinc"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default Meta;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
 import theme from "theme";
+import Meta from "components/Meta";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Meta />
       <Component {...pageProps} />
     </ThemeProvider>
   );
